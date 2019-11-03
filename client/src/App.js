03edcf5b02f667d9f08345f68a25323aea83dde4
Edel Marcelino <edelm8@gmail.com>
2019-11-02 17:38:22 -0700

    init folder structure

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import TestApp from "./pages/TestApp";
+
+function App() {
+  return (
+      <div>
+        <TestApp />
+      </div>
+  );
+}
+
+export default App;
---
+++ client/src/pages/TestApp.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export class TestApp extends Component {
+    render() {
+        return (
+            <div>
+                <h1>My TEST APP</h1>
+                <p>HELLO WORLD!!!</p>
+            </div>
+        )
+    }
+}
+
+export default TestApp
