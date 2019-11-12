f7dbe0ba56192065c1d3a666d175ff39e8c1da21
Edel Marcelino <edelm8@gmail.com>
2019-11-11 23:02:07 -0800

    added: Private Route component

--- client/src/App.js
+++ client/src/App.js
@@ -3,24 +3,38 @@
 import React from "react";
 import AppBar from "./components/AppBar";
 import { useAuth0 } from "./react-auth0-spa";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Button from "@material-ui/core/Button";
+import TestApp from "./pages/TestApp";
+import PrivateRoute from "./components/PrivateRoute";
+import Link from "@material-ui/core/Link";
 
 function App() {
-  
   // const { isAuthenticated, loginWithRedirect, logout, loading, user } = useAuth0();
   const { loading } = useAuth0();
 
   if (loading) {
-    return (
-      <div>Loading...</div>
-    );
+    return <div>Loading...</div>;
   }
 
+  const preventDefault = event => event.preventDefault();
+
   return (
-    <div className="App">
-      <header>
-        <AppBar />
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header>
+          <AppBar />
+        </header>
+        <Link href="/TestApp">
+          protected page
+        </Link>
+        <Switch>
+          <PrivateRoute exact path="/TestApp" component={TestApp} />
+          {/* <Route exact path="/"/>
+          <Route exact path="/TestApp" component={TestApp}/> */}
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
