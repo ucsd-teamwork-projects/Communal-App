73a28fa67d0f0e44fb5263dd7946551fb66c4e1f
matteuc <ymc006@ucsd.edu>
2019-11-14 19:54:26 -0800

    new landing

---
+++ routes/api/user.js
@@ -0,0 +1,26 @@
+const router = require("express").Router();
+const userController = require("../../controllers/userController");
+
+// Matches with "/api/user/"
+router
+  .route("/")
+  .post(userController.create)
+
+// Matches with "/api/user/:id"
+router
+  .route("/:id")
+  .get(userController.find)
+  .update(userController.update)
+  .delete(userController.delete)
+
+router
+  .route("/:id/likes")
+  .post(userController.pushLikes)
+  // .delete(userController.pullLikes)
+
+router
+  .route("/:id/dislikes")
+  .post(userController.pushDisikes)
+  // .delete(userController.pullDislikes)
+
+module.exports = router;
