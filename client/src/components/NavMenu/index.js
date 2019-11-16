3252865dada5babde24e30506845702de6bd55fd
edelBCS <edelm8@gmail.com>
2019-11-16 13:33:26 -0800

    hid navmenu items when not auth'ed

--- client/src/components/NavMenu/index.js
+++ client/src/components/NavMenu/index.js
@@ -4,7 +4,7 @@ import { useAuth0 } from "../../react-auth0-spa";
 import { Navbar, Nav, NavDropdown, Form } from 'react-bootstrap';
 
 function NavMenu() {
-  const { user } = useAuth0();
+  const { user, isAuthenticated } = useAuth0();
   return (
     <Navbar style={{"zIndex": "1000"}} bg="primary" expand="lg">
       <Navbar.Brand href="/">Communal</Navbar.Brand>
@@ -12,11 +12,15 @@ function NavMenu() {
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
           <Nav.Link href="/">Home</Nav.Link>
-          <Nav.Link href="/profile">My Profile</Nav.Link>
-          <NavDropdown title="Socials" id="basic-nav-dropdown">
-            <NavDropdown.Item href="/find-social">Explore Socials</NavDropdown.Item>
-            <NavDropdown.Item href="/add-social">Add New Social</NavDropdown.Item>
-          </NavDropdown>
+          {isAuthenticated && (
+            <React.Fragment>
+              <Nav.Link href="/profile">My Profile</Nav.Link>
+              <NavDropdown title="Socials" id="basic-nav-dropdown">
+                <NavDropdown.Item href="/find-social">Explore Socials</NavDropdown.Item>
+                <NavDropdown.Item href="/add-social">Add New Social</NavDropdown.Item>
+              </NavDropdown>
+            </React.Fragment>
+          )}
         </Nav>
         <Form inline>
           <span className="mr-2">{user?"Welcome, " + user.name:""}</span>
