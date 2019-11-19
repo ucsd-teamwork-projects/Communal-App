7f948889a257cb32479b05e1655d9905d3b9120e
jmg1478 <johnathanmg1478@gmail.com>
2019-11-18 20:30:30 -0800

    landing page edits and adds

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,8 +1,15 @@
 import React, { Component } from "react";
 import { Container } from "react-bootstrap";
 import { Row } from "react-bootstrap";
-import { Col, Card } from "react-bootstrap";
+import { Col, Card, Carousel, Badge } from "react-bootstrap";
 import Logo from "../assets/img/logo.png"
+import Balboa from "../assets/img/Balboa.jpg"
+import Midway from "../assets/img/midway.png"
+import SunsetCliffs from "../assets/img/SunsetCliffs.jpg"
+import BorregoFlowers from "../assets/img/BorregoFlowers.jpg"
+import MountLaguna from "../assets/img/MountLaguna.jpg"
+import Lajollacaves from "../assets/img/Lajollacaves.jpeg"
+
 
 function Landing() {
   return (
@@ -39,6 +46,100 @@ function Landing() {
               <Card.Footer className="text-muted"></Card.Footer>
             </Card>
           </Col>
+          <Col></Col>
+        </Row>
+      </Container>
+      <br>
+      </br>
+      {/* Carousel */}
+      <Container>
+        <Row>
+          <Col></Col>
+          <Col xs={10}><Carousel>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src={Midway}
+      width="300"
+      height="500"
+      alt="First slide"
+    />
+    <Carousel.Caption>
+      <h1 ><Badge pill variant="dark">Midway Museum</Badge></h1>
+      <h3>Over 115,000 Socialized Yearly</h3>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src={Balboa}
+      width="300"
+      height="500"
+      alt="Second slide"
+    />
+
+    <Carousel.Caption>
+      <h1><Badge pill variant="dark">Balboa Park</Badge></h1>
+      <h3>Over 1,000,000 Socializes Yearly</h3>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src={SunsetCliffs}
+      width="300"
+      height="500"
+      alt="thirs slide"
+    />
+
+    <Carousel.Caption>
+      <h1><Badge pill variant="dark">Sunset Cliffs</Badge></h1>
+      <h3>Over 100 Socialized Daily</h3>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src={BorregoFlowers}
+      width="300"
+      height="500"
+      alt="forth slide"
+    />
+
+    <Carousel.Caption>
+      <h1><Badge pill variant="dark">Borrego Springs Flowers</Badge></h1>
+      <h3>Over 50,000 Socialized Yearly</h3>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src={MountLaguna}
+      width="300"
+      height="500"
+      alt="fith slide"
+    />
+
+    <Carousel.Caption>
+      <h1><Badge pill variant="dark">Mount Laguna</Badge></h1>
+      <h3>Over 150 Socialized Daily</h3>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src={Lajollacaves}
+      width="300"
+      height="500"
+      alt="sixth slide"
+    />
+
+    <Carousel.Caption>
+      <h1 ><Badge pill variant="dark">La Jolla Caves</Badge></h1>
+      <h3>Over 40,000 Socialized Yearly</h3>
+    </Carousel.Caption>
+  </Carousel.Item>
+</Carousel></Col>
           <Col></Col>
         </Row>
       </Container>
