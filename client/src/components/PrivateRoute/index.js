b65431a331107175a493bfc8010ea2120c108ad2
Edel Marcelino <edelm8@gmail.com>
2019-11-19 23:29:58 -0800

    heroku

--- client/src/components/PrivateRoute/index.js
+++ client/src/components/PrivateRoute/index.js
@@ -6,7 +6,6 @@ const PrivateRoute = ({ component: Component, path, user, ...rest }) => {
   const { loading, isAuthenticated, loginWithRedirect } = useAuth0();
 
   useEffect(() => {
-    console.log("is Authed: ", isAuthenticated)
     if (loading || isAuthenticated) {
       return;
     }
