9736693bb525ddcd0b80b384011bab1a305e2bcf
edelBCS <edelm8@gmail.com>
2019-11-21 18:44:49 -0800

    testing heroko Autho0

--- client/src/index.js
+++ client/src/index.js
@@ -13,15 +13,14 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { Auth0Provider } from "./react-auth0-spa";
 import config from "./auth_config.json";
+import History from "./utils/History";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 // A function that routes the user to the right place
 // after login
 const onRedirectCallback = appState => {
-    window.history.replaceState(
-        {},
-        document.title,
+    History.push(
         appState && appState.targetUrl
             ? appState.targetUrl
             : window.location.pathname
