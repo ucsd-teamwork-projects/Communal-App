ac06d508a7981c25ecc954d3f08be9a8526847b5
Colin Cason <colinjcason@gmail.com>
2019-11-21 21:30:03 -0800

    mergin with master/updated about page

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import "./index.css";
 
 export default function About() {
     return (
-        <div>
-            About Page
+        <div className="about">
+                <div className="container-about">
+                    <h1>What the heck is <span className="logo">Communal</span>!?</h1>
+                    <h4>What is a Social!?</h4>
+                    <h4>What's with the bee!?</h4>
+                    <p>We're glad you asked.</p>
+                    <p>Communal is a social app designed to bring people together through exploring their cities.</p>
+                    <p>Imagine swiping through a dating app without the empty feeling inside.</p>
+                    <p>Communal brings people together through what we call Socials, basically events you join or create. Through Socials, you get to explore your city and hopefully make some new friends along the way!</p>
+                    <p>You can discover hobbies, clubs, speakeasys and more!</p>
+                    <p>Much like bees, people need community and we hope to facilitate those gatherings with Communal.</p>
+                </div>
         </div>
     )
 }
